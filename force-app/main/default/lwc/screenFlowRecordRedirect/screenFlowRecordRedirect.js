3463f0153fad2906c260722fb67cdd84ec82859d
BrandonEnclude <56023143+BrandonEnclude@users.noreply.github.com>
2026-05-07 14:09:02 +0200

    Simplify redirection logic in ScreenFlowRecordRedirect
    Removed validation for objectApiName and recordId before redirecting.

--- force-app/main/default/lwc/screenFlowRecordRedirect/screenFlowRecordRedirect.js
+++ force-app/main/default/lwc/screenFlowRecordRedirect/screenFlowRecordRedirect.js
@@ -3,12 +3,8 @@ import { LightningElement, api } from 'lwc';
 export default class ScreenFlowRecordRedirect extends LightningElement {
    @api objectApiName;
    @api recordId;
-   displayText = 'Redirecting...'
-
+   
    connectedCallback() {
-      if (!this.objectApiName || !this.recordId) {
-         this.displayText = 'Invalid parameters set; cannot redirect'
-      }
       window.location.href = `/lightning/r/${this.objectApiName}/${this.recordId}/view`;
    }
 }
